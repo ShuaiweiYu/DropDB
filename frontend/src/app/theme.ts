db0c773fbbc667cebf672730ad3800787262823f
Shuaiwei Yu <yu.shuaiwei.1999@gmail.com>
2025-04-30 16:55:23 +0200

    customized the theme

--- frontend/src/app/theme.ts
+++ frontend/src/app/theme.ts
@@ -1,11 +1,27 @@
 'use client';
 import { createTheme } from '@mui/material/styles';
 
+const colors = {
+    color1: '#FF6242',
+    color2: '#FF8142',
+    color3: '#FFA142',
+    color4: '#FFD642',
+    color5: '#FFE99C',
+}
+
 const theme = createTheme({
     // use CSS theme variables
     cssVariables: true,
     typography: {
         fontFamily: 'var(--font-roboto)',
+    },
+    palette:{
+        primary: {
+            main: colors.color2,
+        },
+        secondary: {
+            main: colors.color3,
+        }
     },
 });
 
