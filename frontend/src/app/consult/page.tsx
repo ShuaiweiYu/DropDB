b59f74d4923661707e6fa1f023845e7b72cc4f8c
shuaiwei_yu <yu.shuaiwei.1999@gmail.com>
2025-05-01 17:35:46 +0200

    add consult page placeholder

---
+++ frontend/src/app/consult/page.tsx
@@ -0,0 +1,13 @@
+import Button from "@mui/material/Button";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>Profile</h1>
+            <p>This is the profile page.</p>
+            <Button variant="text">Text</Button>
+            <Button variant="contained">Contained</Button>
+            <Button variant="outlined">Outlined</Button>
+        </div>
+    );
+}
